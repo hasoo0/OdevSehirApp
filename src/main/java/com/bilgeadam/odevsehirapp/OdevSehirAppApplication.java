ea431c1eaad2de7f30e3dad4cbb5cb73d6e3859f
hasan huseyin <hkizilay65@gmail.com>
2022-02-21 21:29:11 +0300

    sehir bolumu yazildi

---
+++ src/main/java/com/bilgeadam/odevsehirapp/OdevSehirAppApplication.java
@@ -0,0 +1,41 @@
+package com.bilgeadam.odevsehirapp;
+
+import com.bilgeadam.odevsehirapp.entity.Sehir;
+import com.bilgeadam.odevsehirapp.repository.SehirRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OdevSehirAppApplication implements CommandLineRunner {
+
+    @Autowired
+    private SehirRepository sehirRepository;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdevSehirAppApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        Sehir sehir1 = new Sehir();
+        sehir1.setAd("Ankara");
+
+        Sehir sehir2 = new Sehir();
+        sehir2.setAd("Samsun");
+
+        Sehir sehir3 = new Sehir();
+        sehir3.setAd("Mardin");
+
+        sehirRepository.save(sehir1);
+        sehirRepository.save(sehir2);
+        sehirRepository.save(sehir3);
+
+
+    }
+}
---
+++ src/main/java/com/bilgeadam/odevsehirapp/controller/SehirController.java
@@ -0,0 +1,56 @@
+package com.bilgeadam.odevsehirapp.controller;
+
+import com.bilgeadam.odevsehirapp.entity.Sehir;
+import com.bilgeadam.odevsehirapp.exception.SehirNotFoundException;
+import com.bilgeadam.odevsehirapp.repository.SehirRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class SehirController
+{
+
+    @Autowired
+    private SehirRepository sehirRepository;
+
+    @GetMapping("/sehir/{sehirId}")
+    public Sehir getSehir(@PathVariable("sehirId") long id)
+    {
+        Sehir sehir = null;
+        Optional<Sehir> sehirDB =  sehirRepository.findById(id);
+
+        if (sehirDB.isPresent())
+        {
+            sehir = sehirDB.get();
+            return sehir;
+        }
+        else
+        {
+            throw  new SehirNotFoundException(id + "nolu sehir bulunamadi!");
+        }
+    }
+
+    @GetMapping("/sehir")
+    public List<Sehir> getTumSehirler()
+    {
+        return sehirRepository.findAll();
+    }
+
+    @GetMapping("/sehir/ara/ad/{ad}")
+    public Sehir findSehirByAd(@PathVariable("ad") String ad)
+    {
+
+        Sehir sehir = sehirRepository.findByAd(ad);
+        return sehir;
+    }
+
+
+
+}
+
+
